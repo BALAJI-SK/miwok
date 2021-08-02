4d7eb26688b941041d4d0c1bb818b2ec21393759
S K BALAJI <skbalajimbl1@gmail.com>
2021-08-02 16:46:18 +0530

    Add Images and Edited WordAdapter,Word data Structure and added few Styling.

--- app/src/main/java/com/example/miwok/Word.java
+++ app/src/main/java/com/example/miwok/Word.java
@@ -1,10 +1,19 @@
 package com.example.miwok;
 
 public class Word {
-   private   String Default="",miwok="";
+    private  int imageId=hasImage;
+    private   String Default="",miwok="";
+    //Act's like a flag
+    private static final int  hasImage=-1;
+    Word(String Default,String miwok,int imageId){
+        this.Default = Default;
+        this.miwok = miwok;
+        this.imageId= imageId;
+    }
     Word(String Default,String miwok){
         this.Default = Default;
         this.miwok = miwok;
+
     }
     public  String getMiwokTranslation(){
         return  miwok;
@@ -12,4 +21,10 @@ public class Word {
     public  String getDefaultTranslation(){
         return Default;
     }
+   public  int getImageResourceId(){return imageId;}
+
+   public  boolean hasImageResourceId(){
+
+        return imageId != hasImage;
+   }
 }
--- app/src/main/java/com/example/miwok/WordAdapter.java
+++ app/src/main/java/com/example/miwok/WordAdapter.java
@@ -5,10 +5,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 
 import java.util.ArrayList;
 
@@ -16,27 +19,47 @@ public class WordAdapter  extends ArrayAdapter<Word> {
 //    public WordAdapter(@NonNull Context context, int resource, ArrayList<Word> textViewResourceId) {
 //        super(context, 0, textViewResourceId);
 //    }
-WordAdapter(Context context ,ArrayList<Word> textViewResourceId){
+   private  final int   categorycolor ;
+WordAdapter(Context context, ArrayList<Word> textViewResourceId, int category_color){
+
     super(context, 0, textViewResourceId);
+    this.categorycolor=category_color;
 }
 
     @NonNull
     @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent ) {
         View listview = convertView;
-      // this we use to change
+      // this we use to change or inflate so it looks like a reusing ListView
         if(listview == null) {
             listview= LayoutInflater.from(getContext()).inflate(
                     R.layout.list_item, parent, false);
         }
         //Here we get class object location in the list
         Word pos = getItem(position);
+
+
         // Find the TextView in the list_item.xml layout with the ID version_name
         TextView miwok = listview.findViewById(R.id.miwok_textView);
         TextView Defalut = listview.findViewById(R.id.default_textView);
+        ImageView exampleImage = listview.findViewById(R.id.exampleImage);
+        LinearLayout linearLayout = listview.findViewById(R.id.linearTextLayouts);
+
         //set that String to the list
         miwok.setText(pos.getMiwokTranslation());
         Defalut.setText(pos.getDefaultTranslation());
+
+        // It sets the background
+        int color = ContextCompat.getColor(getContext(),categorycolor);
+        linearLayout.setBackgroundColor(color);
+
+        if(pos.hasImageResourceId()){
+        exampleImage.setImageResource(pos.getImageResourceId());
+        // This line because we are reusing the View so we need to set Visible so it won't create problem
+        exampleImage.setVisibility(View.VISIBLE);}
+        else {
+            exampleImage.setVisibility(View.GONE);
+        }
 
 
         return  listview;
